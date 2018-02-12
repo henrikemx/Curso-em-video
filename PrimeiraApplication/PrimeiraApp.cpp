4dcaa5c6cdd324545026920409341ddbfe19c7f8
Henrique Maximiliano Macedo <hmaxmacedo@gmail.com>
2018-02-12 10:10:10 -0200

    Adicionado primeiro projeto em C++

---
+++ PrimeiraApplication/PrimeiraApp.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	cout << "#=================================#" << endl;
+	cout << "# Primeira aplicação feita em C++ #" << endl;
+	cout << "#=================================#" << endl;
+
+}
