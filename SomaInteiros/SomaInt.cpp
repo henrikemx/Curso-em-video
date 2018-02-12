d903ee2ad325ab8250e35531316870ea1e9005f4
Henrique Maximiliano Macedo <hmaxmacedo@gmail.com>
2018-02-12 10:03:06 -0200

    Adicionado projeto SomaInteiros

---
+++ SomaInteiros/SomaInt.cpp
@@ -0,0 +1,19 @@
+/*
+ * SomaInt.cpp
+ *
+ *  Created on: 10 de fev de 2018
+ *      Author: kryptonian
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int x = 10;
+	int y = 25;
+	int z = x + y;
+
+	cout << endl;
+	cout << x << " + " << y << " = " << z;
+}
